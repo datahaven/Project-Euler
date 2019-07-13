74fc175fc6c9f18ae9a84585d563212f16ec6958
Adrian Dale <adrian@adriandale.com>
2019-07-13 22:55:27 +0100

    Added Euler_387

---
+++ Euler_387/Euler_387.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Euler_387
+{
+    public class Euler_387
+    {
+        private List<List<long>> m_Harshads;
+        private List<long> m_StrongHarshads;
+
+        public void BuildHarshads(int maxDigits)
+        {
+            m_Harshads = new List<List<long>>();
+            List<long> singleDigits = new List<long>();
+            for(long i=1; i<=9; ++i)
+            {
+                singleDigits.Add(i);
+            }
+            m_Harshads.Add(singleDigits);
+
+            for(int d=2; d<=maxDigits; ++d)
+            {
+                List<long> harshads = new List<long>();
+                int digitsCount = m_Harshads[d - 2].Count;
+                for (int i=0; i<digitsCount; ++i)
+                {
+                    // Try each digit on the end of the previous harshad
+                    for(long nd=0; nd<=9; ++nd)
+                    {
+                        long newNo = m_Harshads[d - 2][i] * 10 + nd;
+                        
+                        if (newNo % DigitSum(newNo) == 0)
+                        {
+                            harshads.Add(newNo);
+                        }
+                    }
+                }
+                m_Harshads.Add(harshads);
+            }
+        }
+
+        private long DigitSum(long num)
+        {
+            long sum = 0;
+            do
+            {
+                sum += num % 10;
+                num /= 10;
+            }
+            while (num  != 0);
+            return sum;
+        }
+
+        public void FindStrongHarshads()
+        {
+            m_StrongHarshads = new List<long>();
+
+            for (int i = 0; i < m_Harshads.Count; ++i)
+            {
+                for (int j = 0; j < m_Harshads[i].Count; ++j)
+                {
+                    long harshad = m_Harshads[i][j];
+                    long digitSum = DigitSum(harshad);
+                    if (harshad % digitSum == 0 && IsPrime(harshad / digitSum))
+                    {
+                        m_StrongHarshads.Add(harshad);
+                    }
+                }
+            }
+        }
+
+        public void SumHarshadPrimes(long limit)
+        {
+            long sum = 0;
+            for (int i = 0; i < m_StrongHarshads.Count; ++i)
+            {
+                // Try each digit on the end of the harshad
+                // (it won't end in 0,2,4,6,8)
+                for (long nd = 1; nd <= 9; nd+=2)
+                {
+                    long newNo = m_StrongHarshads[i] * 10 + nd;
+                    if (newNo < limit && IsPrime(newNo))
+                    {
+                        sum += newNo;
+                    }
+                }   
+                
+            }
+
+            Console.WriteLine(sum.ToString());
+        }
+
+        private bool IsPrime(long n)
+        {
+            if (n <= 3)
+            {
+                return n > 1;
+            }
+            else if (n % 2 == 0 || n % 3 == 0)
+            {
+                return false;
+            }
+
+            long i = 5;
+            while (i * i <= n)
+            {
+                if ((n % i == 0) || (n % (i + 2) == 0))
+                {
+                    return false;
+                }
+                i = i + 6;
+            }
+            return true;
+        }
+    }
+}
---
+++ Euler_387/Program.cs
@@ -0,0 +1,24 @@
+﻿// Project Euler 387 - Harshad Numbers
+// Adrian Dale
+// 13/07/2019
+
+// Congratulations, the answer you gave to problem 387 is correct.
+//
+// You are the 3853rd person to have solved this problem.
+//
+// This problem had a difficulty rating of 10%. The highest difficulty rating you have solved so far is 100%
+using System;
+
+namespace Euler_387
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Euler_387 e387 = new Euler_387();
+            e387.BuildHarshads(13);
+            e387.FindStrongHarshads();
+            e387.SumHarshadPrimes(100000000000000);
+        }
+    }
+}
